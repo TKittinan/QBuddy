d6ad9a4f61425fd87246a6c9981bcdb4a279d1bc
TKittinan <tkittinan.work@gmail.com>
2026-03-03 22:59:16 +0700

    Update

---
+++ frontend-web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-blue-500">
+      <h1 className="text-white text-lg font-bold">Edit src/App.tsx to edit this screen.</h1>
+    </div>
+  )
+}
+
+export default App
