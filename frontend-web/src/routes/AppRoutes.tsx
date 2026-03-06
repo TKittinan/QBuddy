fab94c6fccd1a1dcd759c109d436b0d8df5e1a4f
TKittinan <tkittinan.work@gmail.com>
2026-03-06 19:42:08 +0700

    Update

--- frontend-web/src/routes/AppRoutes.tsx
+++ frontend-web/src/routes/AppRoutes.tsx
@@ -1,50 +1,4 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import DashboardLayout from "../layout/DashboardLayout";
-import AuthLayout from "../layout/AuthLayout";
-import ProtectedRoute from "./ProtectedRoute";
-import LiveQueue from "../pages/LiveQueue";
-import LiveQueueLayout from "../layout/LiveQueueLayout";
-import UserManagement from "../pages/UserManagement";
-import UserManageLayout from "../layout/UserManageLayout";
-
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<LiveQueueLayout />}>
-          <Route path="/livequeue" element={<LiveQueue />} />
-        </Route>
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<UserManageLayout />}>
-          <Route path="/usermanage" element={<UserManagement />} />
-        </Route>
-      </Route>
-      
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-  );
-};
-
-export default AppRoutes;
-=======
-// Layouts
 import DashboardLayout from "../layout/DashboardLayout";
 import LiveQueueLayout from "../layout/LiveQueueLayout";
 import UserManageLayout from "../layout/UserManageLayout";
@@ -81,4 +35,3 @@ export default function AppRoutes() {
     </Routes>
   );
 }
->>>>>>> main
