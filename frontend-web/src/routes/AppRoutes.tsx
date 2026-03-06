e3fb181e424b36c7770245b2b6825f710a476ac5
TKittinan <tkittinan.work@gmail.com>
2026-03-06 19:35:45 +0700

    fix merge conflict

--- frontend-web/src/routes/AppRoutes.tsx
+++ frontend-web/src/routes/AppRoutes.tsx
@@ -1,54 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import DashboardLayout from "../layout/DashboardLayout";
-import AuthLayout from "../layout/AuthLayout";
-import ProtectedRoute from "./ProtectedRoute";
-import LiveQueue from "../pages/LiveQueue";
-import LiveQueueLayout from "../layout/LiveQueueLayout";
-import UserManagement from "../pages/UserManagement";
-import UserManageLayout from "../layout/UserManageLayout";
-
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<LiveQueueLayout />}>
-          <Route path="/livequeue" element={<LiveQueue />} />
-        </Route>
-      </Route>
-
-      <Route element={<ProtectedRoute />}>
-        <Route element={<UserManageLayout />}>
-          <Route path="/usermanage" element={<UserManagement />} />
-        </Route>
-      </Route>
-      
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-  );
-};
-
-export default AppRoutes;
-=======
 // Layouts
 import DashboardLayout from "../layout/DashboardLayout";
 import LiveQueueLayout from "../layout/LiveQueueLayout";
 import UserManageLayout from "../layout/UserManageLayout";
-import PlaceManagementLayout from "../layout/PlacemanagementLayout"; // ✅ ชื่อใหม่
+import PlaceManagementLayout from "../layout/PlacemanagementLayout";
 // Pages
 import Dashboard from "../pages/Dashboard";
 import LiveQueue from "../pages/LiveQueue";
@@ -74,11 +29,10 @@ export default function AppRoutes() {
 
       {/* ✅ เส้นทางสำหรับ Place Management */}
       <Route element={<PlaceManagementLayout />}>
-        <Route path="/placemanagement" element={<PlaceManagement />} />
+        <Route path="/reservations" element={<PlaceManagement />} />
       </Route>
 
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
 }
->>>>>>> main
