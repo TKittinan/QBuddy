a50314c7772e48e2bce11fb2324809796581c0d2
ชื่อของคุณ <hayatekungtar@gmail.com>
2026-04-05 19:51:36 +0700

    Update  user_service.ts
    to show "role" admin,user when running

--- backend/src/services/user_service.ts
+++ backend/src/services/user_service.ts
@@ -1,7 +1,20 @@
 import { prisma } from "../lib/prisma";
 
-export const getAllUsers = () => {
-  return prisma.user.findMany();
+export const getAllUsers = async () => {
+  const users = await prisma.user.findMany({
+    include: {
+      admin: true,
+    },
+  });
+
+  // map เผื่อโชว์ role ออกมาด้วย
+  return users.map((u) => ({
+    user_id: u.user_id,
+    email: u.email,
+    name: u.name,
+    password: u.password,
+    role: u.admin ? u.admin.role : "user",
+  }));
 };
 
 export const getUserById = async (id: number) => {
