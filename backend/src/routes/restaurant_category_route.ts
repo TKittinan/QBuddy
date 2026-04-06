8bec87bb73ffdb75ef137a86ba0e5044fcd04b2b
ชื่อของคุณ <hayatekungtar@gmail.com>
2026-04-06 22:38:33 +0700

    Config restaurant_controller_category

--- backend/src/routes/restaurant_category_route.ts
+++ backend/src/routes/restaurant_category_route.ts
@@ -4,14 +4,22 @@ import {
   removeCategoryFromRestaurant,
   getCategoriesOfRestaurant,
 } from "../controllers/restaurant_category_controller";
+import { authMiddleware } from "../middlewares/auth_middleware";
+import { adminMiddleware } from "../middlewares/admin_middleware";
 
 const router = Router();
 
 // เพิ่ม category ให้ร้าน
-router.post("/", addCategoryToRestaurant);
+router.post("/", 
+  authMiddleware,
+  adminMiddleware,
+  addCategoryToRestaurant);
 
 // ลบ category ออกจากร้าน
-router.delete("/", removeCategoryFromRestaurant);
+router.delete("/", 
+  authMiddleware,
+  adminMiddleware,
+  removeCategoryFromRestaurant);
 
 // ดู category ของร้าน
 router.get("/:restaurant_id", getCategoriesOfRestaurant);
