600b8c89fbdc634f973f7f035db8b30caf5160a0
ชื่อของคุณ <hayatekungtar@gmail.com>
2026-04-17 05:09:40 +0700

    Create auth_middleware.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,8 +5,10 @@ import ticket_routes from './modules/tickets/ticket_routes';
 import party_routes from './modules/party/party_routes';
 import support_routes from './modules/support/support_routes';
 import settings from './modules/settings/settings_routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json()); 
 
 // ลงทะเบียนเส้นทางของ User
