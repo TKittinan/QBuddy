223005e40e0e6175dfda83ec379c0c73164e13f4
tee <tkittinan.work@gmail.com>
2026-04-18 15:39:08 +0700

    Fix connfig to deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,5 +26,5 @@ app.use('/api/dashboard', dashboard_routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
